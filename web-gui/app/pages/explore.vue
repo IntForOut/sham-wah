88fc44b53971827c90bdfa11193ce7a6b0fafdff
vpech77 <vanessa.pech77@gmail.com>
2026-01-23 17:45:51 +0100

    feat: add raw map canvas

--- web-gui/app/pages/explore.vue
+++ web-gui/app/pages/explore.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="h-full p-6">
     <div class="h-full flex flex-col">
-      <!-- Page Header -->
-      <div class="mb-4">
-        <h1 class="text-xl font-bold text-gray-900 dark:text-white mb-1">
-          Graph Exploration
-        </h1>
-      </div>
-
-      <!-- Graph Canvas -->
       <div class="flex-1 min-h-0">
         <GraphCanvas />
       </div>
